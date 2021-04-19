c86163715b254dab7089090baa08c879644c8efd
root <root@localhost.localdomain>
2021-04-19 14:37:57 +0300

    world time app complete

--- world_time/lib/main.dart
+++ world_time/lib/main.dart
@@ -5,6 +5,7 @@ import 'package:world_time/pages/location.dart';
 
 void main() {
   runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
     initialRoute: "/",
     routes:{
       '/': (context) => Loading(),

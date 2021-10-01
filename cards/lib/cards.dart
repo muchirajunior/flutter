ce56d9eb4348f8574b4c27d53fb265b7de939038
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2021-10-01 16:58:25 +0300

    flutter cards complete sample :tada: :smiley: :star: :star:

--- cards/lib/cards.dart
+++ cards/lib/cards.dart
@@ -2,53 +2,216 @@ import 'package:flutter/material.dart';
 
 class Cards extends StatelessWidget {
   const Cards({ Key? key }) : super(key: key);
-
+  
+  
   @override
   Widget build(BuildContext context) {
+    var screenHeight= MediaQuery.of(context).size.height;
+    var screenWidth= MediaQuery.of(context).size.width;
+    var data=[1,2,3,4,5,6,7,8];
+
+
     return Scaffold(
      appBar: AppBar(
        title: Text("cards screen"),
      ),
      body:ListView(
-       padding: EdgeInsets.all(10),
+       padding: EdgeInsets.all(5),
        children: [
 
+        //card 1
          Card(
-           elevation: 10,
-           
+           elevation: 10, 
            child: Row(
              children:[
               Container(
-                width: 200,
-                height: 100,
+                width:screenWidth*0.4,
+                height: 135,
                 color: Colors.grey,
                 child: Icon(Icons.image),
               ),
-                Container(
-                padding: EdgeInsets.all(10),
-                color: Colors.white,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                      Text("The  body text of our card  goes  within this space ",
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
+                Expanded(
+                  child: Container(
+                  padding: EdgeInsets.all(10),
+                  color: Colors.white,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        child: Text("The  body text of our card   goes  within this space The  body text of our card   goes  within this space The  body text of our card   goes  within this space",
+                        maxLines: 3,
+                        overflow: TextOverflow.ellipsis,
+                         ),
+                      ),
+                    SizedBox(height:20),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.end,
                       children: [
                         TextButton(onPressed: (){}, 
-                        child: Text("Read More >>", style: TextStyle(color: Colors.amber),
-                        ))
-                      ],)
-                  ],
-                ),
-              ), 
+                          child: Text("Read More >>", style: TextStyle(color: Colors.amber),
+                          )),
+                      ],
+                    )
+                    ],
+                  ),
+                              ),
+                ), 
              ]
            ),
          ),
 
+     SizedBox(height: 20,),
+      
+      //card 2
+      Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          //card 2 copy 1
+           Card(
+             elevation: 10,
+             child: Column(
+               children: [
+                 Container(
+                   height: 100,
+                   width: screenWidth*0.55,
+                   color: Colors.grey,
+                   child: Icon(Icons.image),
+                 ),
 
+                 Container(
+                   padding: EdgeInsets.all(20),
+                   child: Text("This is my title text"),),
+                 SizedBox(height: 20,),
+                 SizedBox(
+                   height: 35,
+                   width: 100,
+                   child: RaisedButton(
+                    color: Colors.amber,
+                    splashColor: Colors.blue,
+                    animationDuration: Duration(seconds:4),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                    onPressed: (){},
+                    child: Text("Add Item",
+                      style: TextStyle(color: Colors.white),
+                    ), ),
+                 ),
+                 SizedBox(height: 10,)
+               ],
+             ),
+           ),
+          
+          
+        ],
+      ),
+
+
+
+    // card 3
+       
+    Card(
+        elevation: 10,
+        child: Column(
+          children: [
+            Container(
+              height: 200,
+              width: double.infinity,
+              color: Colors.grey,
+              child: Icon(Icons.image),
+            ),
+
+            Container(
+              padding: EdgeInsets.all(20),
+              child: Text("This is the blog sample text, This is the blog sample text ",
+               maxLines: 3,
+              ),),
+            SizedBox(height: 20,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: TextButton(onPressed: (){}, 
+                  child: Text("Read More >>", style: TextStyle(color: Colors.amber),)),
+                ),
+              ],
+            )
+          ],
+          ),
+        ),
+
+
+        //card 4
+
+        Card(
+          elevation:10,
+          child: Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Text("  cartegories",  style: TextStyle(color: Colors.amber, fontSize: 22),),
+                ],
+              ),
+            
+              GridView.count( 
+                padding: EdgeInsets.all(15),
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                mainAxisSpacing:10,
+                crossAxisSpacing: 10,
+                crossAxisCount: 4,
+                childAspectRatio: 0.8,
+                children: data.map((item) => Column(
+                  children: [
+                    CircleAvatar(
+                      child: Icon(Icons.image, size: 20,),
+                    ),
+                    Text("type $item")
+                  ],
+                )).toList(),
+                ),
+               Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextButton(onPressed: (){}, 
+                      child: Text("More >>", style: TextStyle(color: Colors.amber),)),
+                    ),
+                  ],
+                )
+            ],
+          ),
+        ),
+
+        SizedBox(height: 10,),
+
+        // card 5
+        Card(
+          elevation: 10,
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("The Header ", style: TextStyle(color: Colors.amber, fontSize:20)),
+                    Icon(Icons.data_saver_off)
+                  ],
+                ),
+              ),
+              Container(height: 1, width: double.infinity, color:Colors.grey ),
+              ListTile(
+                leading: CircleAvatar(child: Icon(Icons.image),),
+                title: Text("this is my title"),
+                subtitle: Text("here goes the description of the text, this shouldn't be as much long though"),
+                // trailing: Text(DateTime.now().year.toString(),),
+              ),
+            ],
+          ),
+        )
+          
 
 
        ],
--- cards/lib/home.dart
+++ cards/lib/home.dart
@@ -11,27 +11,27 @@ class Home extends StatelessWidget {
         width:double.infinity ,
         decoration: BoxDecoration(
           color:Colors.blue,
-          // image: DecorationImage(image: NetworkImage("https://mfiles.alphacoders.com/724/724214.jpg'"))
+          image: DecorationImage(image: NetworkImage("https://mfiles.alphacoders.com/724/724214.jpg"), fit: BoxFit.cover)
         ),
        
        child: Column(
          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
          crossAxisAlignment: CrossAxisAlignment.center,
          children: <Widget>[
-          Text("           WELCOME \n \n \n OUR HOME CARD DESIGN  ",
+          SizedBox(height: 30,),
+          Text("WELCOME",style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold)),
+          Text("OUR HOME CARD DESIGN  ",
            style: TextStyle(color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
           ),
           SizedBox(height: 100,),
           SizedBox(
             height: 40,
-            width: 300,
+            width: 200,
             child: RaisedButton(
               color: Colors.amber,
-              splashColor: Colors.blue,
-              animationDuration: Duration(seconds:4),
               shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
               onPressed: ()=>Navigator.pushNamed(context, '/cards'),
-               child: Text("Cards",
+               child: Text("View Cards",
                 style: TextStyle(color: Colors.white),
                ), ),
           ),

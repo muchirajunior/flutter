a642cfc754a2d3f7895ca117330cfabc3ca73b4a
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2021-10-23 13:27:43 +0300

    initial commit :gear:

---
+++ searchbars/android/app/src/main/kotlin/com/example/searchbars/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.searchbars
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ searchbars/lib/searchbars.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class SearchBars extends StatelessWidget {
+  const SearchBars({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}

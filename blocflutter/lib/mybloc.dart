7fdfe8df3227794a22a6756ef863a6a63c894907
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2021-11-16 17:29:44 +0300

    flutter bloc implemented successfully with mutliple objects of different types in the state :gear: :hammer::tada::smiley::star::star:

--- blocflutter/lib/mybloc.dart
+++ blocflutter/lib/mybloc.dart
@@ -8,14 +8,20 @@ class CounterCubit extends Cubit<Map<String,dynamic>> {
   CounterCubit() : super({"count":0,"items":[]});
 
   void increment() { 
-    state['count']+=1;
-    state['items'].add(state['count']);
-    print(state);
-    emit({...state});
+     add(state); //using an external method 
+     emit({...state});
     }
+
   void decrement() {
     state['count']-=1;
     if (state['items'].length>0) state['items'].removeLast();
     print(state);
      emit({...state});}
+}
+
+//an external method to manipulate the state, this helps in writting a cleaner code
+add(var state){
+  state['count']+=1;
+  state['items'].add(state['count']);
+  print(state);
 }

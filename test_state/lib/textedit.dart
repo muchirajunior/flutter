4de5a3a15902efb7e5fb7d7e9de1e1fde09417ea
muchirajunior <muchirajunior022@gmail.com>
2022-09-05 12:40:06 +0300

    added reusable textEdit :gear:

--- test_state/lib/textedit.dart
+++ test_state/lib/textedit.dart
@@ -1,10 +1,21 @@
 import 'package:flutter/material.dart';
 
-customTextEdit(){
-  return  TextField(
-    onChanged: (text)=>print(text),
-    decoration: const InputDecoration(
-      hintText: "enter dummy text"
+class CustomTextEdit extends StatelessWidget {
+  String? text;
+  CustomTextEdit({Key? key, this.text}) : super(key: key);
+  TextEditingController _controller=TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+    padding: const EdgeInsets.all(20.0),
+    child: TextField(
+      controller: _controller,
+      onChanged: (text)=>print(text),
+      decoration: InputDecoration(
+        hintText: text
+      ),
     ),
   );
+  }
 }
